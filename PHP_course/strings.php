3f477f43b96f2b22b61053c6b6f83ad6bf30251d
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2022-02-01 14:23:21 +0500

    next step

---
+++ PHP_course/strings.php
@@ -0,0 +1,7 @@
+<?php
+$name = 'Zura';
+$string = 'Hello I am $name. I am 28';
+$string2 = "Hello I am $name. I am 28";
+echo $string. '<br>';
+echo $string2. '<br>';
+echo 'Hello'.'World'.'and PHP';
